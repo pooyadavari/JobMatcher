da77bf8ad1bdc34a4184ef31700fa3d576364afa
‘Pooya’ <davari.pooya@gmail.com>
2018-10-08 02:21:55 +1100

    Add some basic unit tests to test the repository methods

---
+++ Backend/JobMatcher.Test/RepositoryTests.cs
@@ -0,0 +1,30 @@
+﻿using JobMatcher.Data.DAL;
+using JobMatcher.Data.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobMatcher.Test
+{
+    [TestClass]
+    public class RepositoryTests
+    {
+        [TestMethod]
+        public void TestGetCandidates()
+        {
+            IRepository rep = new Repository();
+            Task<List<Candidate>> candidates = rep.GetCandidates();
+            Assert.IsTrue(candidates.Result.Count > 0);
+        }
+
+        [TestMethod]
+        public void TestGetJobs()
+        {
+            IRepository rep = new Repository();
+            Task<List<Job>> jobs = rep.GetJobs();
+            Assert.IsTrue(jobs.Result.Count > 0);
+        }
+    }
+}
